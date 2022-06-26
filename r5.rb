f8d955a6c45279ac3689283577df87f6ca00b57d
Sumio K <tap.miwo@gmail.com>
2022-06-26 18:45:36 +0900

    レッスン5途中経過

---
+++ r5.rb
@@ -0,0 +1,32 @@
+#条件判断
+# if文
+    # a = 10
+    # b = 20
+
+    # if a > b
+        # puts "aはbより大きい"
+    # elsif a < b
+        # puts "aはbより小さい"
+    # else
+        # puts "aはbと同じ"
+    # end
+
+#unless文(条件がfalseだった場合に処理が実行される)
+    # a = 10
+    # b = 20
+    # unless a > b
+        # puts "aはbより大きくない"
+    # end
+
+#case文
+    tags = ["A" , "IMG" , "PRE"]
+    tags.each do |tagname|
+        case tagname
+        when "P" , "A" , "I" , "B" , "BLOCKQUOTE0"
+            puts "#{tagname} has a child."
+        when "IMG" , "BR"
+            puts "#{tagname} has no child." 
+        else
+            puts "#{tagname} cannnot be used"
+        end
+    end
